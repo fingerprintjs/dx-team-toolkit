8a82bd5902ac4596276fcb6f0acaa8e029689e6f
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-01-18 17:19:07 +0800

    chore: remove jsx flag

--- packages/eslint-config-dx-team/index.js
+++ packages/eslint-config-dx-team/index.js
@@ -11,9 +11,6 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 2020,
     sourceType: 'module',
   },
