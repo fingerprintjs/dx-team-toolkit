d9b48b71997383a462a76b346ffbd176d9b29f9e
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-06-18 12:40:02 +0200

    feat: update footer-max-line-length to 400 characters

--- packages/commit-lint-dx-team/index.js
+++ packages/commit-lint-dx-team/index.js
@@ -3,5 +3,6 @@ module.exports = {
   rules: {
     'body-max-line-length': [2, 'always', 200],
     'header-max-length': [2, 'always', 200],
+    'footer-max-line-length': [2, 'always', 400],
   },
 }
