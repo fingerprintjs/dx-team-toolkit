a109c40c70726b3154d27f1e867f58418babf978
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-09-24 12:36:44 +0200

    test: use unique test package path

--- __tests__/test-utils/testPkgExec.ts
+++ __tests__/test-utils/testPkgExec.ts
@@ -1,8 +1,15 @@
 import * as cp from 'child_process'
 import * as path from 'path'
 import * as os from 'os'
+import humanId from 'human-id'
 
-export const TEST_PACKAGE_PATH = path.resolve(os.tmpdir(), 'test-pkg')
+export const TEST_PACKAGE_PATH = path.resolve(
+  os.tmpdir(),
+  `test-pkg-${humanId({
+    separator: '-',
+    capitalize: false,
+  })}`
+)
 
 export function testPkgExec(command: string) {
   return cp.execSync(command, { cwd: TEST_PACKAGE_PATH })
