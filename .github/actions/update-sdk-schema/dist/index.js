6078f8cd113e63e25c59761681821a96f4efb7b4
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-10-08 12:38:30 +0200

    chore: build action

--- .github/actions/update-sdk-schema/dist/index.js
+++ .github/actions/update-sdk-schema/dist/index.js
@@ -47163,7 +47163,8 @@ async function listReleasesBetween({ octokit, config, fromTag, toTag }) {
         page++;
     }
     console.info(`Found ${releases.length} releases that match following criteria: from ${fromTag} to ${toTag}`);
-    return releases;
+    // Sort releases in ascending order
+    return releases.sort((a, b) => (semver.gt(a.tag_name, b.tag_name) ? 1 : -1));
 }
 async function getReleaseNotes(releaseNotesAsset, allowedScopes, packageName) {
     // Map of changeset file name and contents
