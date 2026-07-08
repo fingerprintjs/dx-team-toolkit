63489b18d0a44c3662490fc17ece4c9d41513cf6
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-07-08 14:20:53 +0200

    refactor: extract and reuse NativePlatformDefinition type in native deps note formatter

--- packages/changesets-native-dependency-format/src/native-dependency/note.ts
+++ packages/changesets-native-dependency-format/src/native-dependency/note.ts
@@ -1,7 +1,9 @@
 import { resolveIOSDependency } from './ios'
 import { resolveAndroidDependency } from './android'
 
-function formatter(platforms: { displayName: string; versionRange: string }[]) {
+export type NativePlatformDefinition = { displayName: string; versionRange: string }
+
+function formatter(platforms: NativePlatformDefinition[]) {
   let result = `### Supported Native SDK Version Range\n\n`
 
   result += platforms
@@ -14,7 +16,7 @@ function formatter(platforms: { displayName: string; versionRange: string }[]) {
 }
 
 export async function generateNativeDepsNote(androidPath: string, iosPodspecPath: string) {
-  const platformVersions: { displayName: string; versionRange: string }[] = [
+  const platformVersions: NativePlatformDefinition[] = [
     {
       displayName: 'Fingerprint iOS SDK',
       versionRange: await resolveIOSDependency({
